ef955cf78cc953213352e126b66d1a1846683b61
Sergii Podriezienko <podreenko1986@gmail.com>
2023-11-28 18:58:56 +0200

    add: item like
    add: item like

--- test-finish-project/src/components/AutoCatalogItem/AutoCatalogItem.js
+++ test-finish-project/src/components/AutoCatalogItem/AutoCatalogItem.js
@@ -1,12 +1,22 @@
 import cardAuto from "../../images/image_test.jpg";
-import like from "../../images/heart_checked.svg";
+import like from "../../images/heart_no_checked.svg";
+import like_checked from "../../images/heart_checked.svg";
+import css from "./AutoCatalogItem.module.css";
+import { useState } from "react";
 
 const AutoCatalogItem = () => {
+  const [isLike, setIsLike] = useState(false);
+
+  function onClickButtonLike() {
+    setIsLike((state) => !state);
+  }
+
   return (
-    <div>
-      <img src={cardAuto} alt="card auto" />
-      <img src={like} alt="like" />
-      <button>cdc</button>
+    <div className={css.itemLike}>
+      <img className={css.imageAuto} src={cardAuto} alt="card auto" />
+      <button onClick={onClickButtonLike} className={css.buttonLike}>
+        <img src={isLike ? like_checked : like} alt="like" />
+      </button>
     </div>
   );
 };
