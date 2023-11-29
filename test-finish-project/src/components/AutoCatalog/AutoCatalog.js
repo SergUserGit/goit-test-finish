2415205646dd0a5e24b203be7511a0286e6ccefd
Sergii Podriezienko <podreenko1986@gmail.com>
2023-11-29 10:47:41 +0200

    add: autoItemPrew
    add: autoItemPrew

--- test-finish-project/src/components/AutoCatalog/AutoCatalog.js
+++ test-finish-project/src/components/AutoCatalog/AutoCatalog.js
@@ -1,6 +1,19 @@
 import AutoCatalogItem from "../AutoCatalogItem/AutoCatalogItem";
 
 const AutoCatalog = () => {
-  return <AutoCatalogItem />;
+  return (
+    <AutoCatalogItem
+      make="Buick"
+      model="Enclave"
+      year={2008}
+      rentalPrice="$40"
+      adressCity="Kiev"
+      adressCountry="Ukraine"
+      rentalCompany="Luxury Car Rentals"
+      type="Premium Suv"
+      idAuto={9582}
+      functionalities="Power liftgate"
+    />
+  );
 };
 export default AutoCatalog;
--- test-finish-project/src/components/AutoCatalogItem/AutoCatalogItem.js
+++ test-finish-project/src/components/AutoCatalogItem/AutoCatalogItem.js
@@ -1,10 +1,22 @@
 import cardAuto from "../../images/image_test.jpg";
 import like from "../../images/heart_no_checked.svg";
 import like_checked from "../../images/heart_checked.svg";
+import split_auto from "../../images/split_auto.svg";
 import css from "./AutoCatalogItem.module.css";
 import { useState } from "react";
 
-const AutoCatalogItem = () => {
+const AutoCatalogItem = ({
+  make,
+  model,
+  year,
+  rentalPrice,
+  adressCity,
+  adressCountry,
+  rentalCompany,
+  type,
+  idAuto,
+  functionalities,
+}) => {
   const [isLike, setIsLike] = useState(false);
 
   function onClickButtonLike() {
@@ -12,10 +24,37 @@ const AutoCatalogItem = () => {
   }
 
   return (
-    <div className={css.itemLike}>
-      <img className={css.imageAuto} src={cardAuto} alt="card auto" />
-      <button onClick={onClickButtonLike} className={css.buttonLike}>
-        <img src={isLike ? like_checked : like} alt="like" />
+    <div className={css.mainDivAuto}>
+      <div className={css.itemLike}>
+        <img className={css.imageAuto} src={cardAuto} alt="card auto" />
+        <button onClick={onClickButtonLike} className={css.buttonLike}>
+          <img src={isLike ? like_checked : like} alt="like" />
+        </button>
+      </div>
+      <div className={css.infoModelAuto}>
+        <p className={css.paragraphInfoAuto}>
+          <span>{make}</span>&nbsp;<span>{model}</span>
+          <span>,</span>&nbsp;<span>{year}</span>
+        </p>
+        <span className={css.spanRentalPrice}>{rentalPrice}</span>
+      </div>
+      <p className={css.paraghDateAuto}>
+        <span className={css.spanAutoInfo}>{adressCity}</span>
+        <img src={split_auto} alt="split auto" />
+        <span className={css.spanAutoInfo}>{adressCountry}</span>
+        <img src={split_auto} alt="split auto" />
+        <span className={css.spanAutoInfo}>{rentalCompany}</span>
+        <img src={split_auto} alt="split auto" />
+        <span className={css.spanAutoInfo}>{type}</span>
+        <img src={split_auto} alt="split auto" />
+        <span className={css.spanAutoInfo}>{model}</span>
+        <img src={split_auto} alt="split auto" />
+        <span className={css.spanAutoInfo}>{idAuto}</span>
+        <img src={split_auto} alt="split auto" />
+        <span className={css.spanAutoInfo}>{functionalities}</span>
+      </p>
+      <button className={css.buttonLeanMore}>
+        <span className={css.spanLeanMore}>Learn more</span>
       </button>
     </div>
   );
